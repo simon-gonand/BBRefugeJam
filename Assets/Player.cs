81b81856635b3c6a4a1ced8cc05e5c4530ada060
Simon <simongonand@gmail.com>
2022-03-06 10:52:44 +0100

    add warning message

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
 
     public static Player instance;
 
+    public float warningMessageDuration;
+
     private void Awake()
     {
         instance = this;
@@ -58,14 +60,14 @@ public class Player : MonoBehaviour
             switch (neighbour.globalRestrictions)
             {
                 case Direction.All:
-                    WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have anything around him", 1.0f);
+                    WarningMessage.instance.Warning("The block " + neighbour.data.blockName + " can't have anything around him", warningMessageDuration);
                     return false;
                 case Direction.UnderAndAbove:
                     foreach (GameObject obj in underabove)
                     {
                         if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
                         {
-                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block above or under him", 1.0f);
+                            WarningMessage.instance.Warning("The block " + neighbour.data.blockName + " can't have another block above or under him", warningMessageDuration);
                             return false;
                         }
                     }
@@ -75,7 +77,7 @@ public class Player : MonoBehaviour
                     {
                         if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
                         {
-                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block next to him", 1.0f);
+                            WarningMessage.instance.Warning("The block " + neighbour.data.blockName + " can't have another block next to him", warningMessageDuration);
                             return false;
                         }
                     }
@@ -85,7 +87,7 @@ public class Player : MonoBehaviour
                     {
                         if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
                         {
-                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block above him", 1.0f);
+                            WarningMessage.instance.Warning("The block " + neighbour.data.blockName + " can't have another block above him", warningMessageDuration);
                             return false;
                         }
                     }
@@ -95,7 +97,7 @@ public class Player : MonoBehaviour
                     {
                         if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
                         {
-                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block under him", 1.0f);
+                            WarningMessage.instance.Warning("The block " + neighbour.data.blockName + " can't have another block under him", warningMessageDuration);
                             return false;
                         }
                     }
@@ -111,35 +113,35 @@ public class Player : MonoBehaviour
                 case Direction.All:
                     if (adjacents.Count > 0 || under.Count > 0 || above.Count > 0)
                     {
-                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block around him", 1.0f);
+                        WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have another block around him", warningMessageDuration);
                         return false;
                     }
                     else break;
                 case Direction.UnderAndAbove:
                     if (under.Count > 0 || above.Count > 0)
                     {
-                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block above or under him", 1.0f);
+                        WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have another block above or under him", warningMessageDuration);
                         return false;
                     }
                     else break;
                 case Direction.Adjacents:
                     if (adjacents.Count > 0)
                     {
-                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block next to him", 1.0f);
+                        WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have another block next to him", warningMessageDuration);
                         return false;
                     }
                     else break;
                 case Direction.Above:
                     if (above.Count > 0)
                     {
-                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block above him", 1.0f);
+                        WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have another block above him", warningMessageDuration);
                         return false;
                     }
                     else break;
                 case Direction.Under:
                     if (under.Count > 0)
                     {
-                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block under him", 1.0f);
+                        WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have another block under him", warningMessageDuration);
                         return false;
                     }
                     else break;
@@ -157,7 +159,7 @@ public class Player : MonoBehaviour
                                 
                                 if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName)
                                 {
-                                    WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " around him", 1.0f);
+                                    WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " around him", warningMessageDuration);
                                     return false;
                                 }
                             }
@@ -165,25 +167,41 @@ public class Player : MonoBehaviour
                         case Direction.UnderAndAbove:
                             foreach (GameObject obj in underabove)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName)
+                                {
+                                    WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " under or above him", warningMessageDuration);
+                                    return false;
+                                }
                             }
                             break;
                         case Direction.Adjacents:
                             foreach (GameObject obj in adjacents)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName)
+                                {
+                                    WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " next to him", warningMessageDuration);
+                                    return false;
+                                }
                             }
                             break;
                         case Direction.Above:
                             foreach (GameObject obj in above)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName)
+                                {
+                                    WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " above him", warningMessageDuration);
+                                    return false;
+                                }
                             }
                             break;
                         case Direction.Under:
                             foreach (GameObject obj in under)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName)
+                                {
+                                    WarningMessage.instance.Warning("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " under him", warningMessageDuration);
+                                    return false;
+                                }
                             }
                             break;
                     }
