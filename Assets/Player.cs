c9468417d6157b0f53d1a183dfe371a699dc7873
Simon <simongonand@gmail.com>
2022-03-06 10:37:47 +0100

    call warning message WIP

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -39,8 +39,6 @@ public class Player : MonoBehaviour
         public List<GameObject> underabove = new List<GameObject>();
     public bool PlacementAllowed()
     {
-        Debug.Log("Hello");
-
         all.Clear();
         underabove.Clear();
         List<GameObject> adjacents = Grid.Instance.GetAdjacentCells(currentlyHoveredCell.posInGrid.x, currentlyHoveredCell.posInGrid.y, currentlyHoveredCell.posInGrid.z);
@@ -60,29 +58,46 @@ public class Player : MonoBehaviour
             switch (neighbour.globalRestrictions)
             {
                 case Direction.All:
+                    WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have anything around him", 1.0f);
                     return false;
                 case Direction.UnderAndAbove:
                     foreach (GameObject obj in underabove)
                     {
-                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName) return false;
+                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
+                        {
+                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block above or under him", 1.0f);
+                            return false;
+                        }
                     }
                     break;
                 case Direction.Adjacents:
                     foreach (GameObject obj in adjacents)
                     {
-                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName) return false;
+                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
+                        {
+                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block next to him", 1.0f);
+                            return false;
+                        }
                     }
                     break;
                 case Direction.Above:
                     foreach (GameObject obj in under)
                     {
-                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName) return false;
+                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
+                        {
+                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block above him", 1.0f);
+                            return false;
+                        }
                     }
                     break;
                 case Direction.Under:
                     foreach (GameObject obj in above)
                     {
-                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName) return false;
+                        if (obj.GetComponent<BaseBlock>().data.blockName == neighbour.data.blockName)
+                        {
+                            WarningMessage.instance.PopMessage("The block " + neighbour.data.blockName + " can't have another block under him", 1.0f);
+                            return false;
+                        }
                     }
                     break;
             }
@@ -94,55 +109,81 @@ public class Player : MonoBehaviour
             switch(currentBlock.globalRestrictions)
             {
                 case Direction.All:
-                    if (adjacents.Count > 0 || under.Count > 0 || above.Count > 0) return false;
+                    if (adjacents.Count > 0 || under.Count > 0 || above.Count > 0)
+                    {
+                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block around him", 1.0f);
+                        return false;
+                    }
                     else break;
                 case Direction.UnderAndAbove:
-                    if (under.Count > 0 || above.Count > 0) return false;
+                    if (under.Count > 0 || above.Count > 0)
+                    {
+                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block above or under him", 1.0f);
+                        return false;
+                    }
                     else break;
                 case Direction.Adjacents:
-                    if (adjacents.Count > 0) return false;
+                    if (adjacents.Count > 0)
+                    {
+                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block next to him", 1.0f);
+                        return false;
+                    }
                     else break;
                 case Direction.Above:
-                    if (above.Count > 0) return false;
+                    if (above.Count > 0)
+                    {
+                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block above him", 1.0f);
+                        return false;
+                    }
                     else break;
                 case Direction.Under:
-                    if (under.Count > 0) return false;
+                    if (under.Count > 0)
+                    {
+                        WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have another block under him", 1.0f);
+                        return false;
+                    }
                     else break;
             }
 
             foreach (ProximityModifier pm in currentBlock.modifiers)
             {
-                if(pm.restrictPlacement)
+                string pmBlockName = pm.block.GetComponent<BaseBlock>().data.blockName;
+                if (pm.restrictPlacement)
                     switch (pm.restrictions)
                     {
                         case Direction.All:
                             foreach (GameObject obj in all)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pm.block.GetComponent<BaseBlock>().data.blockName) return false;
+                                
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName)
+                                {
+                                    WarningMessage.instance.PopMessage("This block " + currentBlock.data.blockName + " can't have " + pmBlockName + " around him", 1.0f);
+                                    return false;
+                                }
                             }
                             break;
                         case Direction.UnderAndAbove:
                             foreach (GameObject obj in underabove)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pm.block.GetComponent<BaseBlock>().data.blockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
                             }
                             break;
                         case Direction.Adjacents:
                             foreach (GameObject obj in adjacents)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pm.block.GetComponent<BaseBlock>().data.blockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
                             }
                             break;
                         case Direction.Above:
                             foreach (GameObject obj in above)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pm.block.GetComponent<BaseBlock>().data.blockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
                             }
                             break;
                         case Direction.Under:
                             foreach (GameObject obj in under)
                             {
-                                if (obj.GetComponent<BaseBlock>().data.blockName == pm.block.GetComponent<BaseBlock>().data.blockName) return false;
+                                if (obj.GetComponent<BaseBlock>().data.blockName == pmBlockName) return false;
                             }
                             break;
                     }
